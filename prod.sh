790aa76ca1167c3c57c9c25d5cd52e0e2ba94661
Qingyang Hu <huqingyang2000@gmail.com>
2019-01-09 10:33:02 +0800

    Fix versioning problem

--- prod.sh
+++ prod.sh
@@ -2,6 +2,5 @@ git submodule update --init --recursive --remote
 npm install
 composer install --ignore-platform-reqs
 ./node_modules/.bin/encore production
-bin/console doctrine:schema:update --force
 bin/console cache:clear
 bin/console cache:warmup --env=prod
